0bf708337dee86f7f2edf9a99fa01fbc1fe166b0
dior <103741636+4dior@users.noreply.github.com>
2025-04-26 17:01:04 +0300

    lol

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 document.getElementById("carForm").addEventListener("submit", async (e) => {
-    e.preventDefault();
-  
-    const budget = parseInt(document.getElementById("budget").value);
-    const type = document.getElementById("type").value;
-  
-    const res = await fetch("cars.json");
-    const cars = await res.json();
-  
-    const resultsDiv = document.getElementById("results");
-    const matches = cars[type].filter(car => car.priceQAR <= budget);
-  
-    resultsDiv.innerHTML = `<h3>Results:</h3>`;
-  
-    if (matches.length > 0) {
-      matches.forEach(car => {
-        resultsDiv.innerHTML += `
-          <div class="car">
-            <strong>${car.name}</strong><br/>
-            ${car.priceQAR.toLocaleString()} QAR
-          </div>
-        `;
-      });
-    } else {
-      resultsDiv.innerHTML += `<p>No cars found under your budget.</p>`;
-    }
-  });
-  
+  e.preventDefault();
+
+  const budget = parseInt(document.getElementById("budget").value);
+  const type = document.getElementById("type").value;
+
+  const res = await fetch("cars.json");
+  const cars = await res.json();
+
+  const resultsDiv = document.getElementById("results");
+  const matches = cars[type].filter(car => car.priceQAR <= budget);
+
+  resultsDiv.innerHTML = `<h3>Results:</h3>`;
+
+  if (matches.length > 0) {
+    matches.forEach(car => {
+      resultsDiv.innerHTML += `
+        <div class="car">
+          <strong>${car.name}</strong><br/>
+          ${car.priceQAR.toLocaleString()} QAR
+        </div>
+      `;
+    });
+  } else {
+    resultsDiv.innerHTML += `<p>No cars found under your budget.</p>`;
+  }
+});
